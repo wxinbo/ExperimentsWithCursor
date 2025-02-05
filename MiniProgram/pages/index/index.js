e9ec94c925f20cdfa1451b4b169544a9245778c4
Xinbo Wang <xinbwang@microsoft.com>
2025-02-05 21:17:50 +0800

    add

---
+++ MiniProgram/pages/index/index.js
@@ -0,0 +1,71 @@
+// index.js
+Page({
+  data: {
+    days: [],
+    marks: [],
+    currentInput: '',
+    currentDate: '',
+    totalDays: 0
+  },
+
+  onLoad: function() {
+    this.initializeCalendar();
+    this.setCurrentDate();
+  },
+
+  initializeCalendar: function() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth(); // 0-11
+    const daysInMonth = new Date(year, month + 1, 0).getDate(); // 获取当前月份的天数
+    const firstDay = new Date(year, month, 1).getDay(); // 获取当前月份第一天是星期几
+
+    const days = [];
+    // 填充空白
+    for (let i = 0; i < firstDay; i++) {
+      days.push({ date: '', selected: false }); // 空白日期
+    }
+    // 填充实际日期
+    for (let i = 1; i <= daysInMonth; i++) {
+      days.push({ date: i, selected: false });
+    }
+    this.setData({ days });
+    this.updateTotalDays();
+  },
+
+  setCurrentDate: function() {
+    const date = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    this.setData({ currentDate: date.toLocaleDateString('zh-CN', options) });
+  },
+
+  selectDay: function(event) {
+    const date = event.currentTarget.dataset.date;
+    const days = this.data.days.map(day => {
+      if (day.date === date) {
+        day.selected = !day.selected;
+      }
+      return day;
+    });
+    this.setData({ days });
+    this.updateTotalDays();
+  },
+
+  updateTotalDays: function() {
+    const totalDays = this.data.days.filter(day => day.selected).length;
+    this.setData({ totalDays });
+  },
+
+  onInput: function(event) {
+    this.setData({ currentInput: event.detail.value });
+  },
+
+  addMark: function() {
+    if (this.data.currentInput) {
+      this.setData({
+        marks: [...this.data.marks, this.data.currentInput],
+        currentInput: ''
+      });
+    }
+  }
+});
